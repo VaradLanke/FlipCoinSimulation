b35afb9d12bb7c46cb026f4962dc26552b1f5d20
VaradLanke <lanke.varad@gmail.com>
2020-05-01 13:47:00 +0530

    <Refactor> : Coin flipped for ten times and counted heads & tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 echo "Welcome to flip coin simulator"
-rn=$((RANDOM%2))
-echo $rn
-if [ $rn -eq 0 ]
-then
-	echo "Tails"
-else
-	echo "Heads"
-fi
+hcount=0
+tcount=0
+for count in {1..10..1}
+do
+	rn=$((RANDOM%2))
+	if [ $rn -eq 0 ]
+	then
+		echo "Tails"
+		tcount=$(($tcount+1))
+	else
+		echo "Heads"
+		hcount=$(($hcount+1))
+	fi
+done
+echo "Heads Won : $hcount"
+echo "Tails Won : $tcount"
